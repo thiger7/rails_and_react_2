e1290957edd3528f5384aeb3dc7a1e624885a237
adf0423 <adf0423@gmail.com>
2023-01-21 16:07:06 +0900

    fix: App.jsの修正

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -6,41 +6,64 @@ import TodoList from './TodoList'
 import EditTodo from './EditTodo'
 import './App.css'
 
-const Nabvar = styled.nav
+const Nabvar = styled.nav`
   background: #dbfffe;
   min-height: 8vh;
   display: flex;
   justify-content: space-arround;
   align-items: center;
+`
 
-const Logo = styled.div
+const Logo = styled.div`
   font-weight: bold;
   font-size: 23px;
   letter-spacing: 3px;
+`
 
-constNavItems = styled.ul
+const NavItems = styled.ul`
   display: flex;
   width: 400px;
   max-width: 40%;
   justify-content: space-around;
-  list-styled: none;
+  list-style: none;
+`
 
-const NavItem = styled.li
+const NavItem = styled.li`
   font-size: 19px;
   font-weight: bold;
   opacity: 0.7;
   &:hover {
     opacity: 1;
   }
+`
 
-const Wrapper = styled.div
+const Wrapper = styled.div`
   width: 700px;
   max-width: 85%;
   margin: 20px auto;
+`
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Nabvar>
+        <Logo>
+          TODO
+        </Logo>
+        <NavItems>
+          <NavItem>
+            <Link to="/todos">
+              Todos
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/todos/new">
+              Add New Todo
+            </Link>
+          </NavItem>
+        </NavItems>
+      </Nabvar>
+    </>
   )
 }
 
